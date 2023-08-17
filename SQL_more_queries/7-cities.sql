eb1f123f7c6f18e3213f8e8d3bcc7a7bc6841855
doreennjagi <jaydenrishi@gmail.com>
2023-08-18 00:13:02 +0300

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,9 +2,9 @@
 -- creates the database hbtn_0d_usa and the table cities
 -- If the database hbtn_0d_usa already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id INT NOT NULL AUTO_INCREMENT UNIQUE,
+
+CREATE TABLE IF NOT EXISTS  hbtn_0d_usa.cities(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	state_id NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FORIEGN KEY (state_id) REFERENCES states(id)
